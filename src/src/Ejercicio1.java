90d62c714fe9d965d14d0159351d14bcded6945f
user.name <valerymunozmorales@gmaiil.com>
2026-07-27 06:57:36 -0500

    Agregue el Ejemplo 1 - Validacion de contraseña

--- src/src/Ejercicio1.java
+++ src/src/Ejercicio1.java
@@ -1,6 +1,17 @@
+import javax.swing.JOptionPane;
+
 public class Ejercicio1 {
-    public static void main (String [] args){
+    public static void main(String[] args) {
 
+        String contrasenaCorrecta = "1234";
+        String contrasena;
 
+        do {
+            contrasena = JOptionPane.showInputDialog("Ingrese la contraseña:");
+
+        } while (!contrasena.equals(contrasenaCorrecta));
+
+        JOptionPane.showMessageDialog(null, "Acceso permitido");
     }
 }
+//el .equals es para comparar dos cadenas de texto y para comparar los numeros usamos ==
