c9db1baeaa4dab5ab55aee2f4fb1b52219dae658
user.name <valerymunozmorales@gmaiil.com>
2026-07-27 17:33:36 -0500

    Agrego el ejemplo 5 - Registro de Estaturas

--- src/src/Ejercicio5.java
+++ src/src/Ejercicio5.java
@@ -1,4 +1,30 @@
 import javax.swing.*;
 
 public class Ejercicio5 {
-   
+
+        public static void main(String[] args) {
+
+            double estatura;
+            double suma = 0;
+            double promedio;
+            int cantidad = 0;
+            int respuesta;
+
+            do {
+
+                estatura = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la estatura del estudiante:"));
+
+                suma = suma + estatura;
+                cantidad++;
+
+                respuesta = Integer.parseInt(JOptionPane.showInputDialog( "¿Desea registrar otra estatura?\n1. Sí\n2. No"));
+
+            } while (respuesta != 2);
+
+            promedio = suma / cantidad;
+
+            JOptionPane.showMessageDialog(null, "Cantidad de estudiantes: " + cantidad +
+                            "\nPromedio de estaturas: " + promedio);
+
+        }
+    }
