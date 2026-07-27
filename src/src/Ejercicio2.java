4ed690e57b9ebc07724f5a901d4cbdb24cd2701e
user.name <valerymunozmorales@gmaiil.com>
2026-07-27 08:21:15 -0500

    Agregue el Ejemplo 2 - Registro de notas

--- src/src/Ejercicio2.java
+++ src/src/Ejercicio2.java
@@ -1,2 +1,32 @@
+import javax.swing.*;
+
 public class Ejercicio2 {
-}
+    public static void main (String [] args){
+
+        double nota;
+        double suma = 0;
+        double promedio ;
+        int cantidad = 0;
+
+        do {
+
+            nota = Double.parseDouble(JOptionPane.showInputDialog("Ingrese una nota (-1 para terminar) : "));
+
+            if ( nota!= -1){
+                suma = suma + nota;
+                cantidad++;
+            }
+
+            } while ( nota!= -1);
+
+        if ( cantidad > 0 ) {
+            promedio = suma / cantidad;
+
+            JOptionPane.showMessageDialog(null, "Cantidad de notas: " + cantidad + "\nSuma: " + suma + "\nPromedio: " + promedio);
+
+        }else{
+            JOptionPane.showMessageDialog(null, "No se ingresaron las notas. ");
+
+         }
+        }
+    }
