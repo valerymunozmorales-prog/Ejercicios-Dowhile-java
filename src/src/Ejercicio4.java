2e2f123726f52dc9dade215ed5301a421a1d86ea
user.name <valerymunozmorales@gmaiil.com>
2026-07-27 17:12:54 -0500

    Agrego el Ejemplo 4 - Registro de ventas

--- src/src/Ejercicio4.java
+++ src/src/Ejercicio4.java
@@ -1,2 +1,24 @@
+import javax.swing.JOptionPane;
+
 public class Ejercicio4 {
+    public static void main(String[] args) {
+
+        double venta;
+        double total = 0;
+        int cantidad = 0;
+        int respuesta;
+
+        do {
+            venta = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor de la venta: "));
+
+            total = total + venta;
+            cantidad++;
+
+            respuesta = Integer.parseInt(JOptionPane.showInputDialog("¿Desea registrar otra venta?\n1. Sí\n2. No"));
+
+        } while (respuesta != 2);
+
+        JOptionPane.showMessageDialog(null, "Ventas registradas: " + cantidad +
+                        "\nTotal vendido: $" + total);
+    }
 }
